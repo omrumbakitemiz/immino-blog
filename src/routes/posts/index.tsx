d695a35ab06e419761f6f58fc4a7c146edc68af8
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-01-26 00:22:39 +0300

    version updates

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <div>
-      <h1 class="font-bold">🍁🍁 Posts 🍁🍁</h1>
+      <h1 class="font-bold text-center">🍁🍁 Posts 🍁🍁</h1>
     </div>
   );
 });
