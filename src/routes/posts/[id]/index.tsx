64124f2a45a936fa9efcb7c86e3410fd5aecbb37
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-02-19 23:32:20 +0300

    add head items

--- src/routes/posts/[id]/index.tsx
+++ src/routes/posts/[id]/index.tsx
@@ -1,6 +1,16 @@
 import { component$ } from '@builder.io/qwik';
 import { loader$ } from '@builder.io/qwik-city';
 
+export const head = {
+  title: 'Posts',
+  meta: [
+    {
+      name: 'posts',
+      content: 'List all posts',
+    },
+  ],
+};
+
 export const usePostLoader = loader$(async (ctx) => {
   const postId = ctx.params.id;
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
