de9df9d3bfc4d4fe72339f177bfcbd68cf778ec8
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-02-19 23:41:32 +0300

    using dynamic head

--- src/routes/posts/[id]/index.tsx
+++ src/routes/posts/[id]/index.tsx
@@ -1,21 +1,12 @@
 import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
 import { loader$ } from '@builder.io/qwik-city';
-
-export const head = {
-  title: 'Posts',
-  meta: [
-    {
-      name: 'posts',
-      content: 'List all posts',
-    },
-  ],
-};
 
 export const usePostLoader = loader$(async (ctx) => {
   const postId = ctx.params.id;
   const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
   const post = await response.json();
-  return { postId, post };
+  return { post };
 });
 
 export default component$(() => {
@@ -24,10 +15,28 @@ export default component$(() => {
   return (
     <>
       <h1 class="text-center">
-        Post ID is: <i>{post.value.postId}</i>
+        Post ID is: <i>{post.value.post.id}</i>
       </h1>
 
       <p>{post.value.post.body}</p>
     </>
   );
 });
+
+export const head: DocumentHead = ({ params, resolveValue }) => {
+  const postData = resolveValue(usePostLoader);
+
+  return {
+    title: `Post "${params.postId}"`,
+    meta: [
+      {
+        name: 'description',
+        content: postData.post.title.slice(0, 25),
+      },
+      {
+        name: 'id',
+        content: params.productId,
+      },
+    ],
+  };
+};
