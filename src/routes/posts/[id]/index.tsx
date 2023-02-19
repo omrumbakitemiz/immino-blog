3c76ee1f2961a746dc6513f79ee6a8968d11786a
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-02-19 23:26:44 +0300

    jsonplaceholder integration added

--- src/routes/posts/[id]/index.tsx
+++ src/routes/posts/[id]/index.tsx
@@ -1,14 +1,23 @@
 import { component$ } from '@builder.io/qwik';
 import { loader$ } from '@builder.io/qwik-city';
 
-export const usePostLoader = loader$(async (ctx) => ({ postId: ctx.params.id }));
+export const usePostLoader = loader$(async (ctx) => {
+  const postId = ctx.params.id;
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+  const post = await response.json();
+  return { postId, post };
+});
 
 export default component$(() => {
   const post = usePostLoader();
 
   return (
-    <h1 class="text-center">
-      Post ID is: <i>{post.value.postId}</i>
-    </h1>
+    <>
+      <h1 class="text-center">
+        Post ID is: <i>{post.value.postId}</i>
+      </h1>
+
+      <p>{post.value.post.body}</p>
+    </>
   );
 });
