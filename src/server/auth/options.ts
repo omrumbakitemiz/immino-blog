1ae5d93979e6252ba75aac9459c1e4a7ccc6d877
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2023-02-21 22:14:20 +0300

    github auth removed

--- src/server/auth/options.ts
+++ src/server/auth/options.ts
@@ -1,23 +1,18 @@
 import type { AuthConfig } from '@auth/core';
 import Credentials from '@auth/core/providers/credentials';
-import Github from '@auth/core/providers/github';
 import { env } from '../env';
 
 export const authOptions: AuthConfig = {
   secret: env.NEXTAUTH_SECRET,
   providers: [
-    Github({
-      clientId: env.GITHUB_CLIENT_ID,
-      clientSecret: env.GITHUB_CLIENT_SECRET,
-    }),
     Credentials({
       name: 'Credentials',
       credentials: {
-        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        username: { label: 'Username', type: 'text', placeholder: 'immino' },
         password: { label: 'Password', type: 'password' },
       },
       async authorize(credentials) {
-        if (credentials?.username === 'admin' && credentials?.password === 'admin123') {
+        if (credentials?.username === 'imminosan' && credentials?.password === 'immino2608') {
           return { id: '1', name: credentials?.username };
         }
         return null;
--- src/server/env.ts
+++ src/server/env.ts
@@ -3,8 +3,6 @@ import { z } from 'zod';
 const envScheme = z.object({
   NEXTAUTH_URL: z.string(),
   NEXTAUTH_SECRET: z.string(),
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
 });
 
 if (typeof window !== 'undefined') {
@@ -12,12 +10,9 @@ if (typeof window !== 'undefined') {
 }
 
 const nodeEnv = process.env;
-console.log('here nodeEnv:', nodeEnv);
 const viteEnv = import.meta.env;
 
 export const env = envScheme.parse({
   NEXTAUTH_URL: nodeEnv.NEXTAUTH_URL || viteEnv.VITE_NEXTAUTH_URL,
   NEXTAUTH_SECRET: nodeEnv.NEXTAUTH_SECRET || viteEnv.VITE_NEXTAUTH_SECRET,
-  GITHUB_CLIENT_ID: nodeEnv.GITHUB_CLIENT_ID || viteEnv.VITE_GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET: nodeEnv.GITHUB_CLIENT_SECRET || viteEnv.VITE_GITHUB_CLIENT_SECRET,
 });
